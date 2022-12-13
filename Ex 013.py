77c601fcff1e39bfaaf15448f96c5dc281036f0a
ThiagoFAm <thiagoamaraldv@gmail.com>
2022-12-12 23:12:32 -0400

    Revisão_3_13-12-2022

---
+++ Ex 013.py
@@ -0,0 +1,12 @@
+#Faça um algoritmo que leia o salário de um funcionário e meostre seu novo salário, com 15% de aumento.
+
+def promocao(salario):
+    p = float(0.15) #porcentagem da promoção. Foi posta em decimal para facilitar o calculo.
+
+    promo = float(salario + (salario * p)) #Equação da nova folha de pagamento
+
+    return promo #Retornando o valor do novo salário do funcionário.
+
+salario = float(input('Digite o salário do funcionário: ')) #Entrada do salário.
+novo_salario = promocao(salario) #Definindo o retorno como 'novo_salario'
+print(f'O funcionário recebeu uma promoção de 15%, resultante em R${novo_salario}') #Exibição.
