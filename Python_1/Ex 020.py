4993d7d186ac24935adfa7304c65ba6f2865f293
ThiagoFAm <thiagoamaraldv@gmail.com>
2022-12-15 02:51:00 -0400

    Revisão_5_15-12-2022

--- Python_1/Ex 020.py
+++ Python_1/Ex 020.py
@@ -3,14 +3,14 @@
 
 import random
 
-alunos = ['Carl','Jeff','Erich','Mason'] #Alunos
+alunos = ['Carl','Jeff','Erich','Mason'] #Alunos.
 
-def sort_order(): #Função
+def sort_order(): #Função.
 
-    ord = random.sample(alunos, k=len(alunos))
+    ord = random.sample(alunos, k=len(alunos)) #Método 'sample' da lib random é usado para embaralhar uma ordem.
 
-    print(ord) #Método 'sample' da lib random é usado para embaralhar uma ordem.
+    print(ord) #Imprimindo a ordem gerada.
 
-    return ord
+    return ord #Retorno da variável ordem da apresentação.
 
-order_sort = sort_order()
+order_sort = sort_order() #Armazenando a ordem do sorteio em uma váriavel.
